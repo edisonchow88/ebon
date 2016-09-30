684c56ae1fabd4d097fa120d7d28f0149675f53a
Edison Chow <testkasei@gmail.com>
2016-09-30 16:27:37 +0800

    update modal_trip_archive @ storefront

--- storefront/controller/pages/main/trip.php
+++ storefront/controller/pages/main/trip.php
@@ -13,10 +13,22 @@ class ControllerPagesMainTrip extends AController {
 			$this->extensions->hk_InitData($this, __FUNCTION__);
 		//END
 		
+		//START: set ajax
+			$ajax['trip/ajax_itinerary'] = $this->html->getSecureURL('trip/ajax_itinerary');
+		//END
+		
 		//START: set result
 		//END
 		
 		//START: set modal
+			$this->addChild('modal/trip/archive', 'modal_trip_archive', 'modal/trip/archive.tpl');
+			$this->addChild('modal/trip/new', 'modal_trip_new', 'modal/trip/new.tpl');
+			$this->addChild('modal/trip/load', 'modal_trip_load', 'modal/trip/load.tpl');
+			$this->addChild('modal/trip/save', 'modal_trip_save', 'modal/trip/save.tpl');
+			$this->addChild('modal/trip/delete', 'modal_trip_delete', 'modal/trip/delete.tpl');
+			$this->addChild('modal/trip/remove', 'modal_trip_remove', 'modal/trip/remove.tpl');
+			$this->addChild('modal/trip/share', 'modal_trip_share', 'modal/trip/share.tpl');
+			$this->addChild('modal/trip/quota', 'modal_trip_quota', 'modal/trip/quota.tpl');
 		//END
 		
 		//START: set link
@@ -24,6 +36,9 @@ class ControllerPagesMainTrip extends AController {
 		
 		//START: set variable
 			$this->view->batchAssign($this->data);
+			if(count($result) > 0) { $this->view->assign('result', $result); }
+			if(count($link) > 0) { $this->view->assign('link', $link); }
+			if(count($ajax) > 0) { $this->view->assign('ajax', $ajax); }
 		//END
 		
 		//START: set template 
